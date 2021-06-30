3be4f70ebb1dd52db78555aa5e42c65018ae32b9
Luis Andres Ramirez Matheus <ramirezandres1994@gmail.com>
2021-06-30 10:02:03 -0400

    register cliente and tipocliente models on admin

--- apikaran/cliente/admin.py
+++ apikaran/cliente/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from apikaran.cliente.models import Cliente, TipoCliente
+
+admin.site.register(Cliente)
+admin.site.register(TipoCliente)
